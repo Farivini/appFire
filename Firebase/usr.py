4abddc202f91b45b1a3767151c1fc3e1327a290e
Vinicius farineli freire <64615200+Farivini@users.noreply.github.com>
2021-10-29 12:38:29 -0300

    Add files via upload

--- Firebase/usr.py
+++ Firebase/usr.py
@@ -1,9 +1,7 @@
 controle = open('controle.txt','r')
 
 def admin(user):
-    adm = 'viniciusfarineli@gmail.com'
-    adm1 = 'leandroalrodrigues@gmail.com'
-    adm2 = 'vescheifer@gmail.com'
+   
 
     if user == adm or user == adm1 or user==adm2:
         return  True
